eb48bec667a1304c4abbbb50f9c981e462c0ed22
MijanurRahman40 <mijanurrahman31416@gmail.com>
2020-12-05 13:34:49 +0600

    all files are updated

---
+++ server.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+const path = require('path');
+
+const users = {};
+
+const socketToRoom = {};
+
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
+});
+
+
+// Server static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//     // set static folder
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (request, response) => {
+//         response.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
+
+server.listen(process.env.PORT || 8888, () => console.log('server is running on port 8000'));
+
+
